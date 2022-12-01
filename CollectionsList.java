2dbed335bcb4bbea32de3a1118d7d853217472ae
Sanjay <119519665+sanjay-1304@users.noreply.github.com>
2022-12-01 18:23:57 +0530

    Add files via upload

---
+++ CollectionsList.java
@@ -0,0 +1,35 @@
+package Sanjay;
+
+import java.util.*;
+
+public class CollectionsList {
+
+	public static void 
++
+(String[] args) {
+		List list = new ArrayList();
+		list.add(1);
+		list.add("Name");
+		System.out.println(list);
+		
+		Map map = new HashMap();
+		map.put(1, "Sanjai");
+		map.put(2, "Dhinesh");
+		map.put(3, "Siva");
+		map.put(4, "Dhanush");
+		map.put(1, "uuuuu");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println(map);
+				
+	}
+
+}
